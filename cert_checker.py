365f5bae5e8f3ea4efcc2eb2eeaecb2a68410020
Philip Ratcliffe <phil@philratcliffe.co.uk>
2015-10-26 13:14:35 +0000

    Initial commit

---
+++ cert_checker.py
@@ -0,0 +1,54 @@
+import sys
+import socket
+import ssl
+
+from pprint import pprint
+
+
+def create_context(sock, certfile=None):
+    """Create an SSL context"""
+
+    # We want to get the cert, so let's be flexible on the protocol.
+    context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+
+    context.verify_mode = ssl.CERT_REQUIRED
+
+    # Note that this can fail silently
+    context.set_default_verify_paths()
+
+    if certfile:
+        context.load_cert_chain(certfile)
+
+    return context
+
+
+def main(argv):
+    """Handles command line arguments and ties everything together"""
+
+    hostname = argv[1]
+    port = int(argv[2]) if len(argv) > 2 else 443
+    addr = (hostname, port)
+
+    with socket.create_connection(addr) as s:
+        context = create_context(s)
+
+        # We pass the hostname so that things works correctly if SNI is
+        # used at server. Note, that not all versions of OpenSSL support SNI
+        # so this may fail.
+        with context.wrap_socket(s, server_hostname=hostname) as ssl_sock:
+
+            cert = ssl_sock.getpeercert()
+            der = ssl_sock.getpeercert(binary_form=True)
+            pem = ssl.DER_cert_to_PEM_cert(der)
+
+            try:
+                ssl.match_hostname(cert, hostname)
+            except ssl.CertificateError as ce:
+                print ("Certificate error {}\n".format(str(ce)))
+
+            print(pem)
+            pprint(cert)
+
+
+if __name__ == '__main__':
+    main(sys.argv)

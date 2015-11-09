f9c057ce216204f20c38e1538eef4e8c3dbf29da
Philip Ratcliffe <phil@philratcliffe.co.uk>
2015-11-09 07:51:54 +0000

    Change return type to string

--- utils.py
+++ utils.py
@@ -15,5 +15,5 @@ def get_pubkey_alg_from_openssl_output(openssl_output_str):
     try:
         return PEM_PK_ALG.findall(openssl_output_str)[0]
     except IndexError:
-        return None
+        return "Not found"
 
